fa9149f628b9ccde07537ecc83cb3fc3b27bcc2c
Jaroslav Tavgen <jaroslav.tavgen@gmail.com>
2024-11-19 12:41:10 +0200

    a

---
+++ Read Text Word By Word.js
@@ -0,0 +1,36 @@
+function createElement(name, parent1)
+{
+  return parent1.appendChild(document.createElement(name));
+}
+let tags = what => document.getElementsByTagName(what);
+
+createElement(`textarea`, document.body);
+createElement(`button`, document.body);
+tags(`textarea`)[0].style.height = `400px`;
+tags(`textarea`)[0].style.width = `1000px`;
+tags(`button`)[0].innerHTML = `OK`;
+tags(`button`)[0].addEventListener(`click`, a1);
+let index = 0;
+let sentences = [];
+let words = [];
+tags(`textarea`)[0].focus();
+function a1(event)
+{
+  words = tags(`textarea`)[0].value.split(` `);
+  tags(`textarea`)[0].value = ``;
+  tags(`button`)[0].removeEventListener(`keyup`, a2);
+  document.addEventListener(`keyup`, a2);
+}
+function a2(event)
+{
+  console.log(event.keyCode)
+  if(event.keyCode == 37)
+  {
+    if ( index > 0 ) index--;  
+  }
+  if(event.keyCode == 39)
+  {
+    if ( index < (words.length-1) ) index++;  
+  }
+  tags(`textarea`)[0].value = words[index];
+}
