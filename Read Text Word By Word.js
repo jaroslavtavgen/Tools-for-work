3ed3bcc8502a328460bef6b65231dd60e15885ed
Jaroslav Tavgen <jaroslav.tavgen@gmail.com>
2024-11-19 13:44:18 +0200

    Update Read Text Word By Word.js

--- Read Text Word By Word.js
+++ Read Text Word By Word.js
@@ -6,6 +6,10 @@ let tags = what => document.getElementsByTagName(what);
 
 createElement(`textarea`, document.body);
 createElement(`button`, document.body);
+createElement(`div`, document.body);
+tags(`div`)[0].style.display = `inline-block`;
+tags(`div`)[0].style.height = `1000px`;
+tags(`div`)[0].style.width = `1000px`;
 tags(`textarea`)[0].style.height = `400px`;
 tags(`textarea`)[0].style.width = `1000px`;
 tags(`button`)[0].innerHTML = `OK`;
@@ -18,12 +22,15 @@ function a1(event)
 {
   words = tags(`textarea`)[0].value.split(` `);
   tags(`textarea`)[0].value = ``;
-  tags(`button`)[0].removeEventListener(`keyup`, a2);
+  document.removeEventListener(`keyup`, a2);
   document.addEventListener(`keyup`, a2);
   index = 0;
 }
 function a2(event)
 {
+  if (document.activeElement.tagName.toLowerCase() === 'textarea') {
+    return; // Do nothing if a textarea is focused
+  }
   console.log(event.keyCode)
   if(event.keyCode == 37)
   {
